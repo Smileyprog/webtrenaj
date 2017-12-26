af70063436200c651359a8b4841212fa8fe022cc
Андрей Закатов <andreyzakatov@gmail.com>
2017-12-26 15:21:30 +0300

    Charset

--- Index.php
+++ Index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
 
   <!-- Скрипты -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
